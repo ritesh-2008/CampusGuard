696238f463a62db396d19376b71ff115e5c4a22d
ritesh-2008 <riteshvanivdekar@gmail.com>
2026-08-17 18:09:41 +0530

    incident router

--- server/router/incident.router.js
+++ server/router/incident.router.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { incidents, GetIncidents } from "../controllers/incident.controllers.js";
+import { incidents,GetIncidents } from "../controllers/incident.controllers.js";
 import { requireAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/", requireAuth, incidents);
-router.get("/", requireAuth, GetIncidents);
+router.get("/",requireAuth,GetIncidents)
 
 export default router;
