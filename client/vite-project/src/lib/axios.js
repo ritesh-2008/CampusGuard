406ebe718d4b794ab38f120b9a3038404416bc5c
ritesh-2008 <riteshvanivdekar@gmail.com>
2026-08-24 00:31:38 +0530

    debug:env error

--- client/vite-project/src/lib/axios.js
+++ client/vite-project/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { supabase } from './supabase.js'
 
-export const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000'
+export const API_URL = import.meta.env.VITE_API_URL 
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
